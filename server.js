c622ce0ab8556c60b1157345d3f0f3f516c5049c
MoChips2 <31714669+smedhan1@users.noreply.github.com>
2018-12-24 10:28:07 -0600

    working html

--- server.js
+++ server.js
@@ -15,7 +15,6 @@ app.get("/survey", function(req, res) {
     res.sendFile(path.join(__dirname, "app/public/survey.html"))
 });
 
-
 app.listen(PORT, function() {
     console.log("App listening on " + PORT);
 });
