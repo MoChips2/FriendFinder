1806c95558b8a2126b4fbcc3f4382b3c0ae89a40
MoChips2 <31714669+smedhan1@users.noreply.github.com>
2018-12-23 21:08:16 -0600

    server adjustment

--- server.js
+++ server.js
@@ -9,9 +9,11 @@ app.use(express.json());
 
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "/app/public/home.html"));
-})
+});
 
-
+app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "app/public/survey.html"))
+});
 
 
 app.listen(PORT, function() {
