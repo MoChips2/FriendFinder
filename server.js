c068f76fce60b17049d0c452e7e3b6bcb4f908b6
MoChips2 <31714669+smedhan1@users.noreply.github.com>
2019-01-02 00:16:15 -0600

    fixed POST route

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 var express = require("express");
-
+var bodyParser = require("body-parser");
 
 var app = express();
 
 var PORT = process.env.PORT || 8080;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 require("./app/routing/htmlRoutes")(app);
 require("./app/routing/apiRoutes")(app);
 
 app.listen(PORT, function() {
-    console.log("App listening on " + PORT);
+    console.log("Server listening on: " + PORT);
 });
