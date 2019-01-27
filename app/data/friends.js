9dc23e3fd664d6e0c4d372b18231dd18d3aa335c
MoChips2 <31714669+smedhan1@users.noreply.github.com>
2019-01-26 23:54:16 -0600

    updated functionality, working modal

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,8 +1,8 @@
 var friendsArray = [
     {
-        name: "Rebecca",
-        photo: "",
-        scores: [
+        name: "Beyonce",
+        photo: "https://www.biography.com/.image/ar_8:10%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cg_faces:center%2Cq_auto:good%2Cw_620/MTQ3MDYxMDUwMDQxNDQ0MjQ3/beyonce_2011_photo_kevin_mazur_wireimage_via_getty_images_122716680_promo.jpg",
+        score: [
             2,
             5,
             4,
@@ -17,9 +17,9 @@ var friendsArray = [
     },
 
     {
-        name: "David",
-        photo: "",
-        scores: [
+        name: "Barack Obama",
+        photo: "https://www.dailydot.com/wp-content/uploads/270/4e/48c0490a5fcf030f22649856ed30f389.jpg",
+        score: [
             5,
             4,
             1,
@@ -34,9 +34,9 @@ var friendsArray = [
     },
 
     {
-        name: "Alejandra",
-        photo: "",
-        scores: [
+        name: "Tom Hanks",
+        photo: "https://www.hellomagazine.com/imagenes/celebrities/2018070950096/celebrity-birthdays-9-15-july/0-246-48/tomhanks-z.jpg",
+        score: [
             3,
             1,
             5,
@@ -51,8 +51,8 @@ var friendsArray = [
     },
 
     {
-        name: "John",
-        photo: "",
+        name: "Will Smith",
+        photo: "http://screenpicks.com/wp-content/uploads/2012/05/Will-Smith.jpg",
         score: [
             5,
             3,
@@ -68,8 +68,8 @@ var friendsArray = [
     },
 
     {
-        name: "Delilah",
-        photo: "",
+        name: "Britney Spears",
+        photo: "https://i.pinimg.com/236x/00/66/0b/00660b1734f627588af5e4fdf494d6cc--celebrities-laughing-smile-smile.jpg",
         score: [
             3,
             3,
@@ -85,8 +85,8 @@ var friendsArray = [
     },
 
     {
-        name: "Samson",
-        photo: "",
+        name: "The Rock",
+        photo: "https://i.pinimg.com/originals/07/b8/69/07b8693e2d06d64cc49b85a6b6ac5213.jpg",
         score: [
             5,
             5,
@@ -102,8 +102,8 @@ var friendsArray = [
     },
 
     {
-        name: "Naomi",
-        photo: "",
+        name: "Oprah",
+        photo: "https://a9p9n2x2.stackpathcdn.com/wp-content/blogs.dir/1/files/2013/03/oprah-winfrey-named-most-influential-celebrity-forbes-magazine-black-enterprise.jpg",
         score: [
             4,
             2,
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,19 +1,47 @@
 
-var friendsData = require("../data/friends");
+var friendsArray = require("../data/friends");
 
 
 module.exports = function(app) {
 
 
-    app.get("/api/friends", function (req, res) {
-        
-        res.json(friendsData);
+    app.get("/api/friends", function(req, res) {
+
+        res.json(friendsArray);
     });
 
 
-    app.post("/api/friends", function (req, res) {
+    app.post("/api/friends", function(req, res) {
+        console.log("-----------------------------------")
+        console.log("receiving newpersonData....")
+        console.log(req.body);
 
-        friendsData.push(req.body);
-        res.json(true);
+        var myScore = req.body.score;
+        // My Best Friend
+        var bestFriendScore = 100;
+        var bestFriendName = "";
+        var bestFriendPhoto = "";
+
+        for (i = 0; i < friendsArray.length; i++) {
+            var currentDiff = 0;
+            var friendScore = friendsArray[i].score;
+            console.log(friendScore);
+
+            for (j = 0; j < 10; j++) {
+                currentDiff += Math.abs(myScore[j] - friendScore[j])
+                console.log('dif', currentDiff);
+            }
+
+            // Determine if current friend is best friend
+            if (currentDiff < bestFriendScore) {
+                bestFriendScore = currentDiff;
+                console.log(currentDiff);
+                bestFriendName = friendsArray[i].name
+                bestFriendPhoto = friendsArray[i].photo
+            }
+        }
+
+        friendsArray.push(req.body);
+        res.json({ name: bestFriendName, photo: bestFriendPhoto });
     });
 }
